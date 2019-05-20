a1ed0bf972bd22fdfa6aaa720a76b1a496ed9221
StefanoMazzuka <stefano.mazzuka@gmail.com>
2019-05-20 21:52:29 +0200

    Final

--- app/src/main/java/com/ar/ciu/ciuar/Map.kt
+++ app/src/main/java/com/ar/ciu/ciuar/Map.kt
@@ -2,24 +2,17 @@ package com.ar.ciu.ciuar
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 
 class Map : AppCompatActivity() {
 
     lateinit var mapFragment: SupportMapFragment
     lateinit var googleMap: GoogleMap
-    lateinit var dataBase: FirebaseDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
